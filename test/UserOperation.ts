40f3f83d4a3ba080df05da237f18fe8b52201f72
Bruno Jutkoski <bjutkoski@gmail.com>
2024-01-18 12:56:35 -0300

    start

---
+++ test/UserOperation.ts
@@ -0,0 +1,31 @@
+import type * as typ from './solidityTypes';
+
+export type UserOperation = {
+  sender: typ.address;
+  nonce: typ.uint256;
+  initCode: typ.bytes;
+  callData: typ.bytes;
+  callGasLimit: typ.uint128;
+  verificationGasLimit: typ.uint128;
+  preVerificationGas: typ.uint256;
+  maxFeePerGas: typ.uint256;
+  maxPriorityFeePerGas: typ.uint256;
+  paymaster: typ.address;
+  paymasterVerificationGasLimit: typ.uint128;
+  paymasterPostOpGasLimit: typ.uint128;
+  paymasterData: typ.bytes;
+  signature: typ.bytes;
+};
+
+export type PackedUserOperation = {
+  sender: typ.address;
+  nonce: typ.uint256;
+  initCode: typ.bytes;
+  callData: typ.bytes;
+  accountGasLimits: typ.bytes32;
+  preVerificationGas: typ.uint256;
+  maxFeePerGas: typ.uint256;
+  maxPriorityFeePerGas: typ.uint256;
+  paymasterAndData: typ.bytes;
+  signature: typ.bytes;
+};
