ab17715da7d5ef00c8363aa1385a516c9dcb7576
Gabriel Speckhahn <749488+gabspeck@users.noreply.github.com>
2024-03-27 19:00:12 +0000

    Working login screen

--- services/web/src/client/components/Login.tsx
+++ services/web/src/client/components/Login.tsx
@@ -1,7 +1,130 @@
-import React from 'react';
-import { Box, Button, Card, Container, Divider, TextField, Typography } from '@mui/material';
+import React, { useMemo, useState } from 'react';
+import { AccountFactory__factory, GenericERC721__factory } from '@giano/contracts/typechain-types';
+import { Button, Card, CircularProgress, Container, Divider, TextField, Typography } from '@mui/material';
+import { parseAuthenticatorData } from '@simplewebauthn/server/helpers';
+import { decode as cborDecode } from 'cbor-web';
+import { ethers } from 'ethers';
+
+const ES256 = -7;
 
 const Login: React.FC = () => {
+  type User = {
+    account: string;
+    rawId: BufferSource;
+    credentialId: bigint;
+  };
+
+  const provider = useMemo(() => new ethers.WebSocketProvider('ws://localhost:8545'), []);
+  const signer = useMemo(() => new ethers.Wallet('0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80', provider), [provider]);
+  const accountFactory = useMemo(() => AccountFactory__factory.connect('0x5fbdb2315678afecb367f032d93f642f64180aa3', signer), [signer]);
+  const tokenContract = useMemo(() => GenericERC721__factory.connect('0xe7f1725e7734ce288f8367e1bb143e90bb3f0512', signer), [signer]);
+
+  const [user, setUser] = useState(null as User | null);
+  const [loggingIn, setLoggingIn] = useState(false);
+  const [registering, setRegistering] = useState(false);
+
+  const uint8ArrayToUint256 = (array: ArrayBuffer) => {
+    const bytes = new Uint8Array(array.byteLength <= 32 ? array : array.slice(0, 32));
+    const hex = Array.from(bytes)
+      .map((b) => b.toString(16).padStart(2, '0'))
+      .join('');
+    return BigInt('0x' + hex);
+  };
+
+  const getCredential = async (id?: BufferSource, challenge?: BufferSource) => {
+    const params = {
+      challenge: challenge || new TextEncoder().encode('abc'),
+      rpId: window.location.hostname,
+      userVerification: 'preferred',
+      ...(id && {
+        allowCredentials: [
+          {
+            id: id,
+            type: 'public-key',
+          },
+        ],
+      }),
+    };
+    console.log({ params });
+    return (await window.navigator.credentials.get({
+      publicKey: params,
+    })) as PublicKeyCredential & { response: AuthenticatorAssertionResponse };
+  };
+
+  const createUser = async (e) => {
+    e.preventDefault();
+    if (registering) {
+      return;
+    }
+    setRegistering(true);
+    const {
+      username: { value: username },
+    } = e.currentTarget;
+    try {
+      const credential = (await navigator.credentials.create({
+        publicKey: {
+          challenge: new TextEncoder().encode('abc'),
+          authenticatorSelection: {
+            requireResidentKey: true,
+            residentKey: 'required',
+            userVerification: 'required',
+          },
+          rp: {
+            id: window.location.hostname,
+            name: 'Giano',
+          },
+          user: {
+            id: new TextEncoder().encode(username),
+            displayName: username,
+            name: username,
+          },
+          pubKeyCredParams: [
+            {
+              alg: ES256,
+              type: 'public-key',
+            },
+          ],
+          timeout: 60_000,
+        },
+      })) as PublicKeyCredential & { response: AuthenticatorAttestationResponse };
+
+      const attestation = cborDecode(new Uint8Array(credential.response.attestationObject));
+      const authData = parseAuthenticatorData(attestation.authData);
+      const publicKey = cborDecode(authData.credentialPublicKey?.buffer as ArrayBuffer);
+      const [x, y] = [publicKey.get(-2), publicKey.get(-3)];
+      const userId = uint8ArrayToUint256(credential.rawId);
+      await (await accountFactory.createUser(userId, { x, y })).wait();
+    } catch (e) {
+      console.error(e);
+    } finally {
+      setRegistering(false);
+    }
+  };
+
+  const logIn = async (e) => {
+    e.preventDefault();
+    if (loggingIn) {
+      return;
+    }
+    setLoggingIn(true);
+    try {
+      const credential = await getCredential();
+      const userId = uint8ArrayToUint256(credential.rawId);
+      if (credential) {
+        const user = await accountFactory.getUser(userId);
+        if (user.account !== ethers.ZeroAddress) {
+          setUser({
+            account: user.account,
+            rawId: credential.rawId,
+            credentialId: userId,
+          });
+        }
+      }
+    } finally {
+      setLoggingIn(false);
+    }
+  };
+
   return (
     <Container
       sx={{
@@ -26,17 +149,33 @@ const Login: React.FC = () => {
         }}
       >
         <img alt="Giano logo" src="/logo.svg" />
-        <form>
-          <Button variant="contained">Login</Button>
+        <form onSubmit={logIn}>
+          <Button
+            type="submit"
+            disabled={loggingIn}
+            variant="contained"
+            sx={{
+              '&.Mui-disabled': { backgroundColor: 'primary.dark' },
+            }}
+          >
+            {loggingIn ? <CircularProgress size="18px" sx={{ margin: '5px', color: 'white' }} /> : 'Login'}
+          </Button>
         </form>
         <Divider flexItem>
           <Typography>or</Typography>
         </Divider>
-        <form style={{ display: 'flex', flexDirection: 'column', textAlign: 'center', width: '100%', gap: 'inherit' }}>
+        <form onSubmit={createUser} style={{ display: 'flex', flexDirection: 'column', textAlign: 'center', width: '100%', gap: 'inherit' }}>
           <Typography variant="h4">Create account</Typography>
-          <TextField label="Passkey" />
-          <Button variant="contained" disabled>
-            Create account
+          <TextField name="username" label="Passkey" required />
+          <Button
+            type="submit"
+            disabled={registering}
+            variant="contained"
+            sx={{
+              '&.Mui-disabled': { backgroundColor: 'primary.dark' },
+            }}
+          >
+            {registering ? <CircularProgress size="18px" sx={{ margin: '5px', color: 'white' }} /> : 'Create account'}
           </Button>
         </form>
       </Card>
