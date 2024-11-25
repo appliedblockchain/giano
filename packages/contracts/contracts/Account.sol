0b6dc233bccb669b1ebf193d63cc6f3152d9d77e
Gabriel Speckhahn <749488+gabspeck@users.noreply.github.com>
2024-11-25 10:06:37 +0000

    add NatSpec docs

--- packages/contracts/contracts/Account.sol
+++ packages/contracts/contracts/Account.sol
@@ -22,10 +22,17 @@ contract Account is ReentrancyGuard, IERC1271 {
         publicKey = _publicKey;
     }
 
+    /**
+     * Returns the expected challenge for a given call payload
+     * @param call The call parameters to generate the challenge against
+     */
     function getChallenge(Types.Call calldata call) public view returns (bytes32) {
         return keccak256(bytes.concat(bytes20(address(this)), bytes32(currentNonce), bytes20(call.target), bytes32(call.value), call.data));
     }
 
+    /**
+     * Returns the x and y coordinates of the public key associated with this contract
+     */
     function getPublicKey() public view returns (Types.PublicKey memory) {
         return publicKey;
     }
@@ -43,6 +50,11 @@ contract Account is ReentrancyGuard, IERC1271 {
     // solhint-disable-next-line no-empty-blocks
     fallback() external payable {}
 
+    /**
+     * Execute an arbitrary call on a smart contract, optionally sending a value in ETH
+     * @param signed The parameters of the call to be executed
+     * @notice The call parameters must be signed with the key associated with this contract
+     */
     function execute(Types.SignedCall calldata signed) external payable validSignature(bytes.concat(getChallenge(signed.call)), signed.signature) nonReentrant {
         (bool success, bytes memory result) = signed.call.target.call{value: signed.call.value}(signed.call.data);
         if (!success) {
@@ -71,6 +83,9 @@ contract Account is ReentrancyGuard, IERC1271 {
             });
     }
 
+    /**
+     * @inheritdoc IERC1271
+     */
     function isValidSignature(bytes32 messageHash, bytes calldata signature) public view override returns (bytes4 magicValue) {
         if (_validateSignature(bytes.concat(messageHash), signature)) {
             return ERC1271_MAGICVALUE;
