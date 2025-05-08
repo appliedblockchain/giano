d88084a6ddaf8d8e5f15ba86db956a2bc296bc4f
Gabriel Speckhahn <749488+gabspeck@users.noreply.github.com>
2025-05-08 18:05:48 +0100

    wip provider code

--- packages/client/src/wagmi/connector.ts
+++ packages/client/src/wagmi/connector.ts
@@ -1,13 +1,16 @@
+import { type EIP1193Provider } from 'viem';
 import { type ProviderMessage } from 'viem';
 import { createConnector } from 'wagmi';
+import { GianoEIP1193Provider } from '../provider';
 
 // Sample implementation by Coinbas: https://github.com/wevm/wagmi/blob/main/packages/connectors/src/coinbaseWallet.ts
-export const createGianoConnector = (({ chains, emitter }) => {
+const createGianoConnector = createConnector<EIP1193Provider>(({ chains, emitter }) => {
+  const provider: EIP1193Provider = GianoEIP1193Provider;
   return {
     id: 'giano',
     name: 'Giano',
     type: 'custom',
-    connect: async () => {
+    async connect() {
       /* The steps below would probably be performed by the EIP-1193 provider we will need to implement, to mimic Metamask's behavior
 	 1. Show UI prompting for wallet creation OR connecting to existing one
 	 2a. Creation: request creation of a new passkey to the browser and call the factory to deploy a new account,
@@ -16,7 +19,8 @@ export const createGianoConnector = (({ chains, emitter }) => {
 	 the credential ID to the factory
 	 3. Store the User struct returned from the factory in the connector's internal state
 	 */
-      return { accounts: [], chainId: chains[0].id };
+      const accounts = await provider.request({ method: 'eth_requestAccounts' });
+      return { accounts, chainId: chains[0].id };
     },
     disconnect: async () => {
       // Reset connector state
@@ -25,11 +29,8 @@ export const createGianoConnector = (({ chains, emitter }) => {
       // Return the connected account's address
       return Promise.resolve([]);
     },
-    getProvider: async () => {
-      // Return a singleton EIP-1193(https://eips.ethereum.org/EIPS/eip-1193)-compliant provider that will do the actual
-      // low level communication with the contracts. Example implementation by Coinbase for their wallet:  https://github.com/coinbase/coinbase-wallet-sdk/blob/master/packages/wallet-sdk/src/CoinbaseWalletProvider.ts#L22
-	  // Probably this provider is where we would implement stuff like calling a faucet or an endpoint to dispatch encoded tx data to
-      return Promise.resolve({});
+    getProvider: async (): Promise<EIP1193Provider> => {
+      return Promise.resolve(provider);
     },
     getChainId: async () => {
       return Promise.resolve(chains[0].id);
