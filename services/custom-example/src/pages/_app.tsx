b7cd16137798eb30d9b13a0c7b24f2e1554a7e62
zeluisping <zeluis.100@gmail.com>
2025-06-08 19:25:17 +0100

    remove rainbowkit provider

--- services/custom-example/src/pages/_app.tsx
+++ services/custom-example/src/pages/_app.tsx
@@ -4,7 +4,7 @@ import type { AppProps } from 'next/app';
 
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { WagmiProvider } from 'wagmi';
-import { RainbowKitProvider } from '@rainbow-me/rainbowkit';
+// import { RainbowKitProvider } from '@rainbow-me/rainbowkit';
 
 import { config } from '../wagmi';
 
@@ -14,9 +14,9 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <WagmiProvider config={config}>
       <QueryClientProvider client={client}>
-        <RainbowKitProvider>
+        {/* <RainbowKitProvider> */}
           <Component {...pageProps} />
-        </RainbowKitProvider>
+        {/* </RainbowKitProvider> */}
       </QueryClientProvider>
     </WagmiProvider>
   );
