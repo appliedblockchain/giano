c608522e5d278402253cfec40bdaa2e13459e890
Giovanni Pinto <giovanni.pinto@appliedblockchain.com>
2025-07-02 16:04:30 +0200

    Misc fixes

--- packages/connector/src/provider.ts
+++ packages/connector/src/provider.ts
@@ -66,9 +66,9 @@ export type GianoProviderInjection = {
     chainType: ChainType
   }
   onCredentialSignedIn(credential: PublicKeyCredential): Promise<boolean> // method to control if the credential is signed in or not
-  getPublicKeyByCredentialId(idHash: Hash): Promise<{ x: Hex; y: Hex }>
-  onCredentialKey(idHash: Hash, xyVector: { x: Hex; y: Hex }): Promise<void>
-  /** @deprecated */
+  getPublicKeyByCredentialId(rawId: ArrayBuffer): Promise<{ x: Hex; y: Hex }>
+  onCredentialKey(rawId: ArrayBuffer, xyVector: { x: Hex; y: Hex }): Promise<void>
+    /** @deprecated */
   onUserOperationSigned?: (signedUserOp: any) => Promise<any> // optional hook for backend validation and submission
 }
 
