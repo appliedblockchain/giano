68adecbd9c9c604ce86f10da92a2443558155286
Steven Koch <stvkoch@gmail.com>
2025-08-27 10:40:48 +0100

    Remove CredentialKeyMapper contract and related references from the codebase. Update README to reflect the removal. (#71)
    Co-authored-by: Steven Koch <steven.koch@appliedblockchain.com>

--- packages/contracts/ignition/modules/CredentialKeyMapper.ts
+++
@@ -1,7 +0,0 @@
-import { buildModule } from '@nomicfoundation/hardhat-ignition/modules';
-
-export default buildModule('CredentialKeyMapper', (m) => {
-  const credentialKeyMapper = m.contract('CredentialKeyMapper');
-
-  return { credentialKeyMapper };
-});
--- packages/contracts/ignition/modules/index.ts
+++ packages/contracts/ignition/modules/index.ts
@@ -1,10 +1,8 @@
 import { buildModule } from '@nomicfoundation/hardhat-ignition/modules';
-import CredentialKeyMapperModule from './CredentialKeyMapper';
 import GianoAccountFactoryModule from './GianoAccountFactory';
 
 export default buildModule('All', (m) => {
   const gianoAccountFactory = m.useModule(GianoAccountFactoryModule);
-  const credentialKeyMapper = m.useModule(CredentialKeyMapperModule);
 
-  return { ...gianoAccountFactory, ...credentialKeyMapper };
+  return { ...gianoAccountFactory };
 });
